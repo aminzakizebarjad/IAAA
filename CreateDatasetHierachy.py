3396fde7bc87716c6700c7a43e45bb9847ef1235
amin <aminzakizebarjad@gmail.com>
2023-04-16 23:57:13 +0330

    here is new CreateDatasetHierarch

--- CreateDatasetHierachy.py
+++ CreateDatasetHierachy.py
@@ -20,12 +20,37 @@ Path.mkdir(ValPath/'normal', parents=True)
 Path.mkdir(ValPath/'abnormal', parents=True)
 
 
-Labels = pandas.read_csv(filepath_or_buffer='data/labels.csv')
+Labels = pandas.read_csv(filepath_or_buffer='iaaa-data-v2/labels.csv')
 print(Labels)
 # print(type(Labels.columns[0]))
 
 # bar = tqdm.tqdm()
-for img_path in tqdm.tqdm(glob.iglob('data/images/*.dcm')):
+for img_path in tqdm.tqdm(glob.iglob('iaaa-data-v2/DICOM/*.dcm')):
+    img = pydicom.dcmread(img_path)
+    matchRow = Labels[img.SOPInstanceUID == Labels.SOPInstanceUID]
+    # print(matchRow)
+    lbl = matchRow.loc[matchRow.index[0], 'Label']
+    # print(lbl)
+    if random.random()< .1:
+        if lbl == 'normal':
+            shutil.copyfile(Path(img_path).as_posix(), ValPath / 'normal' / Path(img_path).name)
+            # print(Path(img_path).as_posix())
+        else:
+            shutil.copyfile(Path(img_path).as_posix(), ValPath / 'abnormal' / Path(img_path).name)
+    else:
+        if lbl == 'normal':
+            shutil.copyfile(Path(img_path).as_posix(), TrainPath/'normal'/Path(img_path).name)
+            # print(Path(img_path).as_posix())
+        else:
+            shutil.copyfile(Path(img_path).as_posix(), TrainPath/'abnormal'/Path(img_path).name)
+
+
+Labels = pandas.read_csv(filepath_or_buffer='iaaa-data-v3/labels.csv')
+print(Labels)
+# print(type(Labels.columns[0]))
+
+# bar = tqdm.tqdm()
+for img_path in tqdm.tqdm(glob.iglob('iaaa-data-v3/DICOM/*.dcm')):
     img = pydicom.dcmread(img_path)
     matchRow = Labels[img.SOPInstanceUID == Labels.SOPInstanceUID]
     # print(matchRow)
